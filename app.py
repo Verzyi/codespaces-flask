c61578028278a0df6cf48aa9f148fd7803519a53
Christopher James Becker <29790590+Verzyi@users.noreply.github.com>
2023-10-24 21:11:14 +0000

    adding more info to website

--- app.py
+++ app.py
@@ -1,10 +1,11 @@
 from flask import Flask, render_template
 
+
 app = Flask(__name__)
 
 @app.route("/")
 def hello_world():
-    return render_template("index.html", title="Chris Becker")
+    return render_template("index.html", title="Chris Becker", user='chris Becker')
 
 
 
